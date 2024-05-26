d20bdd8b84bed305d49ae2fb07ea36024d26cd01
cauanlimberger <cauanlimberger@gmail.com>
2024-05-26 00:40:00 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme')
+
+    var ClassName = document.body.className;
+    if(ClassName == 'light-theme'){
+        this.textContent = "Dark";
+    }
+
+    else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + ClassName);
+});
+
